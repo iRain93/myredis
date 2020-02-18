a743d83e9c36e41e9832c54166d02c4450176fba
iRain93 <1085594562@qq.com>
2020-02-18 18:48:44 +0800

    Update Demo1.java

--- src/main/java/com/iRain93/myredis/demo/Demo1.java
+++ src/main/java/com/iRain93/myredis/demo/Demo1.java
@@ -1,5 +1,7 @@
 package com.iRain93.myredis.demo;
 
+import redis.clients.jedis.Jedis;
+
 /**
  * 用一句话描述次类
  * @author lu
@@ -8,6 +10,9 @@ package com.iRain93.myredis.demo;
 public class Demo1 {
 
     public static void main(String[] args) {
-
+        // 连接本地的Redis服务
+        Jedis jedis = new Jedis("127.0.0.1",6379);
+        // 查看服务是否运行
+        System.out.println(jedis.ping());
     }
 }
